39c55df901f2d8b729f6d5a562d5e4c2a7046aee
Sebastien Le Digabel <sledigabel@gmail.com>
2021-12-30 21:04:21 +0000

    iterm2 plist but this time it works

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-stow -R zshrc bin tmux nvim karabiner iterm2  -t ~
+stow -R -v zshrc bin tmux nvim karabiner iterm2 -t ~
+cp -r iterm2colors/*.itermcolors "${HOME}/.config/iterm2"
 
 # defaults write com.googlecode.iterm2 DisableWindowSizeSnap -integer 1
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${HOME}/.config/iterm2_plist/"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
