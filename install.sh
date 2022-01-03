73d80afb7f357d94c0dbcd08a242676523be9139
Sebastien Le Digabel <sledigabel@gmail.com>
2022-01-03 19:54:33 +0000

    adding gapSize from Rectangle

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ cp -r iterm2colors/*.itermcolors "${HOME}/.config/iterm2"
 # defaults write com.googlecode.iterm2 DisableWindowSizeSnap -integer 1
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${HOME}/.config/iterm2_plist/"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+defaults write com.knollsoft.Rectangle gapSize 20
 
