dc07b4a2af40f59270c61b42da94c651a61e7209
Sebastien Le Digabel <sledigabel@gmail.com>
2022-01-10 15:37:23 +0000

    fixing completion shortcut

--- nvim/.config/nvim/lua/seb_config/completion.lua
+++ nvim/.config/nvim/lua/seb_config/completion.lua
@@ -4,15 +4,15 @@ local lspkind = require("lspkind")
 lspkind.init()
 
 local cmp = require("cmp")
-local luasnip = require("luasnip")
+-- local luasnip = require("luasnip")
 
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
+-- local has_words_before = function()
+-- 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+-- 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
 
 cmp.setup({
 	snippet = {
@@ -27,7 +27,7 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
 		["<up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
 		["<down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-		["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+		["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false }),
 	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
