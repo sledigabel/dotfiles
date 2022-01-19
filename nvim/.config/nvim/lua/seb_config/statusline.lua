1f8b46a5e4855c3ba7df0705ef7a0a239b4ca4cf
Sebastien Le Digabel <sledigabel@gmail.com>
2022-01-19 16:04:48 +0000

    First changes on lualine

--- nvim/.config/nvim/lua/seb_config/statusline.lua
+++ nvim/.config/nvim/lua/seb_config/statusline.lua
@@ -1,1 +1,22 @@
-require("lualine").setup()
+require("lualine").setup({
+	sections = {
+		lualine_b = {
+			{
+				"branch",
+				icon = "",
+			},
+			"diff",
+			{
+				"diagnostics",
+				sources = { "nvim_diagnostic", "nvim_lsp" },
+				symbols = {
+					error = " ",
+					warn = " ",
+					hint = " ",
+					info = " ",
+				},
+			},
+		},
+		lualine_x = { "filetype" },
+	},
+})
