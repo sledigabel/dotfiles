c83e7bd9de40afa449b91af5480d7a1da93ff6e5
Sebastien Le Digabel <sledigabel@gmail.com>
2022-01-19 16:15:04 +0000

    changing the file look on lualine

--- nvim/.config/nvim/lua/seb_config/statusline.lua
+++ nvim/.config/nvim/lua/seb_config/statusline.lua
@@ -17,6 +17,22 @@ require("lualine").setup({
 				},
 			},
 		},
+		lualine_c = {
+			{
+				"filename",
+				file_status = true, -- Displays file status (readonly status, modified status)
+				path = 0, -- 0: Just the filename
+				-- 1: Relative path
+				-- 2: Absolute path
+
+				shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+				symbols = {
+					modified = " ", -- Text to show when the file is modified.
+					readonly = " ", -- Text to show when the file is non-modifiable or readonly.
+					unnamed = "[No Name]", -- Text to show for unnamed buffers.
+				},
+			},
+		},
 		lualine_x = { "filetype" },
 	},
 })
