8239b35d4e42e634961c35707c67d355e4e9e449
Sebastien Le Digabel <sledigabel@gmail.com>
2022-02-09 16:57:01 +0000

    adding cheat path

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-stow -R -v zshrc bin tmux nvim karabiner iterm2 alacritty -t ~
+stow -R -v zshrc bin tmux nvim karabiner iterm2 alacritty cheat -t ~
 cp -r iterm2colors/*.itermcolors "${HOME}/.config/iterm2"
 
 # defaults write com.googlecode.iterm2 DisableWindowSizeSnap -integer 1
