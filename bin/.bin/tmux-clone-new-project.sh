6927522940a3f14e88cd7adae6ac6a0fa51a901b
Sebastien Le Digabel <sledigabel@gmail.com>
2022-02-10 09:59:57 +0000

    adding new tmux add project

---
+++ bin/.bin/tmux-clone-new-project.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+repo=""
+if [[ "${#}" -eq 1 ]]; then
+  repo=$1
+elif [[ "${#}" -eq 0 ]]; then
+  echo "Repository to clone:"
+  echo -n " "
+  read -r repo
+else
+  echo "Need 0 or 1 argument"
+  exit 1
+fi
+
+
+echo ${#}
+echo "cloning ${repo}"
+
+if echo "${repo}" | grep -q "^https://"; then
+  endpoint_name=$(echo "${repo}" | awk -F '/' '{print $3}')
+elif echo "${repo}" | grep -q "^git@"; then
+  endpoint_name=${repo#git@}
+  endpoint_name=${endpoint_name%:*}
+else
+  echo "Couldn't parse repository ${repo} properly"
+fi
+
+case ${endpoint_name} in
+  "github.com")
+    echo "Cloning in PERSO"
+    pushd ~/dev/perso > /dev/null || exit 2
+    git clone "${repo}"
+    popd > /dev/null || exit 2
+    ;;
+  "github.skyscannertools.net")
+    echo "Cloning in WORK"
+    pushd ~/dev/work || exit 2
+    git clone "${repo}"
+    popd || exit 2
+    ;;
+  *)
+    echo "the server is not recognised"
+    exit 1;;
+esac
+read -p "Press enter to continue"
--- bin/.bin/tmux-list-sessions.sh
+++ bin/.bin/tmux-list-sessions.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-SESSION=`tmux list-sessions | awk -F':' '{print $1}' | fzf --layout=reverse --no-preview`
+SESSION=$(tmux list-sessions | awk -F':' '{print $1}' | fzf --layout=reverse --no-preview)
 
 [ "${SESSION}" == "" ] && exit 1
-tmux switch-client -t ${SESSION}
-
+tmux switch-client -t "${SESSION}"
