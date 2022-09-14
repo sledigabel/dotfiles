8714b79dd0e5e96e2da48dfd3b09d57b29912afd
Sebastien Le Digabel <sledigabel@gmail.com>
2022-09-14 10:17:07 +0100

    Adding a branch selection script
    and updated config for which key

---
+++ bin/.bin/tmux-switch-branch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ -z "${TMUX_SESSION_DIR}" ] && exit 1
+branch=""
+if [[ "${#}" -eq 1 ]]; then
+	branch=$1
+elif [[ "${#}" -eq 0 ]]; then
+	branch=$(git branch --sort=-committerdate | sed -e 's/^\*/ /' | fzf --layout=reverse --no-preview)
+else
+	echo "Need 0 or 1 argument"
+	exit 1
+fi
+
+git checkout "${branch//[[:blank:]]/}"
