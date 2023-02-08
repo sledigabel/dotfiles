873721937960480db656e7d84cbdc5e69a060e5c
Sebastien Le Digabel <sledigabel@gmail.com>
2023-02-08 11:28:59 +0000

    Adding git as a source

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,78 +1,79 @@
 return {
-	{
-		"onsails/lspkind-nvim",
-		config = function()
-			local lspkind = require("lspkind")
-			lspkind.init()
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-calc",
-			"hrsh7th/cmp-emoji",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lua",
-			"saadparwaiz1/cmp_luasnip",
-			"windwp/nvim-autopairs",
-			"onsails/lspkind-nvim",
-			"petertriho/cmp-git",
-			-- dep
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local lspkind = require("lspkind")
+  {
+    "onsails/lspkind-nvim",
+    config = function()
+      local lspkind = require("lspkind")
+      lspkind.init()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-calc",
+      "hrsh7th/cmp-emoji",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "saadparwaiz1/cmp_luasnip",
+      "windwp/nvim-autopairs",
+      "onsails/lspkind-nvim",
+      "petertriho/cmp-git",
+      -- dep
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local lspkind = require("lspkind")
       -- for git
       require("cmp_git").setup()
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				mapping = {
-					-- ["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.close(),
-					["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-					["<up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-					["<down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-					["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false }),
-					["<C-x><C-d>"] = cmp.mapping.complete(),
-				},
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "nvim_lua" },
-					{ name = "luasnip" },
-					{ name = "path" },
-					{ name = "buffer", keyword_length = 5 },
-					{ name = "emoji" },
-					{ name = "calc" },
-				}),
-				formatting = {
-					format = lspkind.cmp_format({
-						with_text = true,
-						menu = {
-							buffer = "[buf]",
-							nvim_lsp = "[LSP]",
-							nvim_lua = "[api]",
-							path = "[path]",
-							luasnip = "[snip]",
-							gh_issues = "[issues]",
-							tn = "[TabNine]",
-						},
-					}),
-				},
-			})
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          -- ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+          ["<up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+          ["<down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+          ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false }),
+          ["<C-x><C-d>"] = cmp.mapping.complete(),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "nvim_lua" },
+          { name = "luasnip" },
+          { name = "path" },
+          { name = "buffer", keyword_length = 5 },
+          { name = "emoji" },
+          { name = "calc" },
+          { name = "git" },
+        }),
+        formatting = {
+          format = lspkind.cmp_format({
+            with_text = true,
+            menu = {
+              buffer = "[buf]",
+              nvim_lsp = "[LSP]",
+              nvim_lua = "[api]",
+              path = "[path]",
+              luasnip = "[snip]",
+              gh_issues = "[issues]",
+              tn = "[TabNine]",
+            },
+          }),
+        },
+      })
 
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
-		end,
-	},
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+    end,
+  },
 }
 
 -- use({ "hrsh7th/nvim-cmp" })
